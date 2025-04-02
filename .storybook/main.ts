da118b520e56b91c7ac66ecda912b84c427b5eb5
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2025-04-02 16:58:17 -0400

    Feat/2025 4 (#16)
    * upgrades
    
    * 3.0.1
    
    ---------
    
    Co-authored-by: Gerrod Schirtzinger <149504119+GerrodSchirtzingerInsight@users.noreply.github.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,17 +2,24 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
     "@storybook/addon-a11y",
-    "@storybook/addon-designs"
+    "@storybook/addon-designs",
+    "@storybook/addon-mdx-gfm",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
   },
 };
 export default config;
