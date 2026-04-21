1aef1c6cd1480bb87078bbb26ea410bad7a6bf9c
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2026-04-21 15:02:12 -0400

    storybook and action updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import {
   defineConfig,
   mergeConfig,
 } from "vitest/config";
-import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
