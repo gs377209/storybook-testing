423fbb138179d08fd35770fd4a4a3d8040590d1d
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2026-04-21 15:28:37 -0400

    Feat/upgrade (#52)
    * fresh install
    
    * storybook and action updates
    
    * permissions
    
    * fix testing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,13 +4,12 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
-    "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
     "@storybook/addon-a11y",
     "@storybook/addon-designs",
-    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-docs",
+    "@storybook/addon-vitest"
   ],
 
   framework: {
